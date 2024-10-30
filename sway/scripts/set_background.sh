2b88274046062bf7d6f0adebc3adb5c1852550d7
lethalsopaper <lethalsopaper@192.168.0.151>
2024-10-29 20:40:22 -0600

    sh convert

--- sway/scripts/set_background.sh
+++ sway/scripts/set_background.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 USERNAME=$(whoami)
-IMAGE_DIR="~/.config/sway/img"
+IMAGE_DIR="$HOME/.config/sway/img"
 USER_IMAGE="$IMAGE_DIR/$USERNAME"
+echo "$USER_IMAGE"
 FALLBACK_IMAGE="$IMAGE_DIR/alternative"
 
-if [[ -f "$USER_IMAGE" ]]; then
+if [ -f "$USER_IMAGE" ]; then
+    echo "lol"
     swaymsg output "*" bg "$USER_IMAGE" fill
 else
+    echo "por"
     swaymsg output "*" bg "$FALLBACK_IMAGE" fill
 fi
