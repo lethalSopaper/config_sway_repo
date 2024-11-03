ef5cbd501df28fcafe896a998e5e1d35cdd60fcd
lethalsopaper <2bluisugartechea@gmail.com>
2024-11-03 12:19:23 -0600

    config wayland session for unnsuported gpu start

--- sway/scripts/screenshot.sh
+++ sway/scripts/screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grim ~/Pictures/Screenshots/screenshot-$(date +%Y-%m-%d-%H%M%S).png
+grim ~/Pictures/screenshots/screenshot-$(date +%Y-%m-%d-%H%M%S).png
 notify-send -u low "SS" "Screen shot taken"
