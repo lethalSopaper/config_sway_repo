09e57f5145f9d218be33d3c42f2a0b5645d96c33
lethalsopaper <2bluisugartechea@gmail.com>
2024-11-05 18:54:38 -0600

    little changes

--- sway/scripts/cut_screenshot.sh
+++ sway/scripts/cut_screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grim -g "$(slurp)" ~/Pictures/screenshots/screenshot-$(date +%Y-%m-%d-%H%M%S).png
+grim -g "$(slurp)" ~/Pictures/Screenshots/screenshot-$(date +%Y-%m-%d-%H%M%S).png
 notify-send -u low "SS" "Snipping shot taken"
